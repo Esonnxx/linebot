e9e6cd281d827372bc0466d1da85ba593c8dc91a
Esonnxx <77544102+Esonnxx@users.noreply.github.com>
2023-09-17 22:11:07 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -61,7 +61,7 @@ def handle_message(event):
         return
     if working_status:
        
-        msg =chatgpt.add_msg(f"HUMAN:{event.message.text} 根據以上這個故事先安慰當事人 並反問他在這段感情中學到了什麼?     並在結尾說 謝謝你的分享這裡是第一周的祭品?\n")
+        msg =chatgpt.add_msg(f"HUMAN:{event.message.text} 根據以上這個故事先安慰當事人 並反問他在這段感情中學到了什麼? 並在結尾說 謝謝你的分享這裡是第一周的祭品?\n")
         reply_msg = chatgpt.get_response().replace("AI:", "", 1)
         chatgpt.add_msg(f"AI:{reply_msg}\n")
         line_bot_api.reply_message(
