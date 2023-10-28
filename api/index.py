57999a01c95fe208fe82d7f65c648b9e118ddf21
Esonnxx <77544102+Esonnxx@users.noreply.github.com>
2023-10-28 16:51:32 +0800

    add persona

--- api/index.py
+++ api/index.py
@@ -20,7 +20,7 @@ def handle_agreement(event):
         working_status = False
         line_bot_api.reply_message(
             event.reply_token,
-            TextMessage(text="可以跟我說說你的故事嗎2?"))
+            TextMessage(text="可以跟我說說你的故事嗎3?"))
         working_status = True
         return
     
@@ -29,7 +29,7 @@ def process_initial_response(event, chatgpt, line_bot_api, received_story):
     text1 = "看起來你是我們需要的客戶呢，讓我們一起超渡灰飛煙滅吧!"
     text2 = "但...超渡需要付出一些代價的...不過不用擔心!我們不需要金錢，只要您向我們分享您的情緒，我們便能將情緒蒐集，您便能獲得靈堂所需的祭品，當集齊七個祭品，就是舉行超度儀式的時刻!"
     text3 = "試著選一個關鍵詞來寫一封分手信吧!請不要發出去，只有你我知道，請選擇一個關鍵詞:熱烈的 平淡的 深刻的 以我們有過XX的戀情，來開頭"
-    chatgpt.add_msg(f"HUMAN:{event.message.text} 根據這個故事先安慰我 並詢問他在這短感情中學到了什麼?\n")
+    chatgpt.add_msg(f"HUMAN:{event.message.text} 現在想請你扮演一個腳色，繁體中文回答對話形式：不要是方法或是條列的建議，用像是朋友一樣的交流、一句的對話，和他一直聊天，陪伴他，偶爾也可以互虧，讓聊天不要那麼沉悶，人設是：葬儀師、個性坦率、厭世、說話方式直接，由於自己本身歷過多段被分手的愛情，從中學到很多關於如何處理自己內在傷痛得經驗，因此想運用自己的情史幫助他人，於是決定創辦情感紓解共49天的服務，用對話聊天的方式讓被分手的人發覺自己的內在存在但不知道或是不願意面對的傷痛。根據這個故事用以上的人設先安慰我 並用剛剛的人設詢問他在這短感情中學到了什麼?\n")
     reply_msg = chatgpt.get_response().replace("AI:", "", 1)
     chatgpt.add_msg(f"AI:{reply_msg}\n")
     reply_arr.append(TextSendMessage(reply_msg))
@@ -46,7 +46,7 @@ def process_initial_response(event, chatgpt, line_bot_api, received_story):
 def process_user_story(event, chatgpt, line_bot_api):
     reply_arr1 = []
     text4 = "你成功集到第一周的祭品了!但著個祭品還很脆弱，穩定的方法就是不要與超度的對象聯繫~這非常重要"
-    chatgpt.add_msg(f"HUMAN:{event.message.text} 根據這個故事先安慰我 ")
+    chatgpt.add_msg(f"HUMAN:{event.message.text} 現在想請你扮演一個腳色，繁體中文回答對話形式：不要是方法或是條列的建議，用像是朋友一樣的交流、一句的對話，和他一直聊天，陪伴他，偶爾也可以互虧，讓聊天不要那麼沉悶，人設是：葬儀師、個性坦率、厭世、說話方式直接，由於自己本身歷過多段被分手的愛情，從中學到很多關於如何處理自己內在傷痛得經驗，因此想運用自己的情史幫助他人，於是決定創辦情感紓解共49天的服務，用對話聊天的方式讓被分手的人發覺自己的內在存在但不知道或是不願意面對的傷痛。根據這個故事用以上的人設安慰我 ")
     reply_msg = chatgpt.get_response().replace("AI:", "", 1)
     reply_arr1.append(TextSendMessage(reply_msg))
     reply_arr1.append(TextSendMessage(text4))
@@ -82,7 +82,6 @@ def handle_message(event):
     if event.message.text == "我同意":
         handle_agreement(event)
         received_story = False
-    
     
     elif working_status:  
         if not received_story:
